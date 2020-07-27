236de9fd685d267a74893658fd2dc15a8c3997fd
Yuntao Bai <ytbai@princeton.edu>
2020-07-27 13:55:42 -0700

    Make Embed class
    Run class method make_embed_csv() to make embed.csv. Initializing an instance of Embed() loads the csv file. Embed().map provides a dict mapping char to embed index.

--- data_factory/make_embed.py
+++ data_factory/make_embed.py
@@ -4,16 +4,35 @@ import pandas as pd
 import os
 
 
-data_dir = "data_factory/data"
+class Embed():
+  data_dir = "data_factory/data"
+  embed_filename = "embed.csv"
 
-file_address = os.path.join(data_dir, "delaney.csv")
-df = pd.read_csv(file_address)
+  def __init__(self):
+    self.get_embed()
+    self.make_map()
 
-char_set = set()
-for i in range(len(df)):
-  for c in df.iloc[i]["SMILES"].strip():
-    char_set.add(c)
+  def get_embed(self):
+    self.embed_df = pd.read_csv(os.path.join(self.data_dir, self.embed_filename))
 
-char_list = sorted(list(char_set))
-compress_df = pd.DataFrame({"embed": range(len(char_list)), "char": char_list})
-compress_df.to_csv(os.path.join(data_dir, "embed.csv"), index=False)
+  def make_map(self):
+    self.map = dict()
+    for index in range(len(self.embed_df)):
+      char = self.embed_df.iloc[index]["char"]
+      embed = self.embed_df.iloc[index]["embed"]
+      self.map[char] = embed
+
+  @classmethod
+  def make_embed_csv(cls):
+    data_file_address = os.path.join(cls.data_dir, "delaney.csv")
+    data_df = pd.read_csv(data_file_address)
+
+    char_set = set()
+    for i in range(len(data_df)):
+      for c in data_df.iloc[i]["SMILES"].strip():
+        char_set.add(c)
+
+    char_list = sorted(list(char_set))
+    compress_df = pd.DataFrame({"embed": range(len(char_list)), "char": char_list})
+    compress_df.to_csv(os.path.join(cls.data_dir, cls.embed_filename), index=False)
+  
