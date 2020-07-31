3f82956fe3c460c47423ff5b2ed835af474d7055
Yuntao Bai <ytbai@princeton.edu>
2020-07-31 15:13:40 -0700

    Data generator now generates downstream target values

--- data_factory/data_gen.py
+++ data_factory/data_gen.py
@@ -4,14 +4,29 @@ import pandas as pd
 import os
 from data_factory.make_embed import *
 
+
 class DelaneyDataset(torch.utils.data.Dataset):
-  def __init__(self, mode):
+  target_map = {"y_esol": 1,
+                "min_deg": 2,
+                "mol_weight": 3,
+                "num_H_donors": 4,
+                "num_rings": 5,
+                "num_rot_bonds": 6,
+                "polar_area": 7,
+                "y_true": 8}
+
+  def __init__(self, mode, target_name = "y_true"):
     self.mode           = mode
     self.set_file_address()
     self.df             = pd.read_csv(self.file_address)
     self.length         = self.df.shape[0]
     self.embed_obj      = Embed()
     self.delaney	= False
+    self.set_target(target_name)
+
+  def set_target(self, target_name):
+    self.target_name = target_name
+    self.target_index = self.target_map[self.target_name]
 
   def set_delaney(self, arg = True):
     self.delaney = arg
@@ -24,16 +39,16 @@ class DelaneyDataset(torch.utils.data.Dataset):
 
   def __getitem__(self, i):
     series              = self.df.iloc[i]
-    y_true              = torch.tensor(series[8]).type(torch.cuda.FloatTensor)
-
+    
     if self.delaney:
+      y_true    	= torch.tensor(series[8]).type(torch.cuda.FloatTensor)
       y_esol		= torch.tensor(series[1]).type(torch.cuda.FloatTensor)
       return y_true, y_esol
     else:
+      target_value        = torch.tensor(series[self.target_index]).type(torch.cuda.FloatTensor)
       smiles            = series[9].strip()
       x                 = self.embed_obj.embed_smiles(smiles).type(torch.cuda.FloatTensor)
-      return x, y_true
-
+      return x, target_value
 
   def __len__(self):
     return self.length
