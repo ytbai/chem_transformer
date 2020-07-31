492ee61f6ab7e2517e52d0b662ddcea6b9231586
Yuntao Bai <ytbai@princeton.edu>
2020-07-31 12:26:52 -0700

    Add test_once() to __init__

--- model_factory/__init__.py
+++ model_factory/__init__.py
@@ -1,5 +1,6 @@
 from model_factory.modules import *
 from model_factory.model_factory import *
 from model_factory.train import *
+from model_factory.test import *
 
 from model_factory.models.ChemTransformer.model import *
