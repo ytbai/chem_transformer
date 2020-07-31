c9112144b36e95dcd68f9d887dc8982f26d6febc
Yuntao Bai <ytbai@princeton.edu>
2020-07-31 15:29:32 -0700

    train_ds() and valid_ds() are for downstream tasks

--- model_factory/train.py
+++ model_factory/train.py
@@ -1,6 +1,44 @@
 import torch
 import numpy as np
 
+def train_ds(model_ds_factory, model_factory, train_dataset, criterion, optimizer):
+  train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size = 1, shuffle = True)
+  model_ds_factory.model.train()
+  model_factory.model.eval()
+  loss_epoch = []
+  for x, target_true in train_dataloader:
+    optimizer.zero_grad()
+    features = model_factory.model.features(x)
+    target_pred = model_ds_factory.model(features)
+    loss = criterion(target_pred, target_true)
+    loss.backward()
+    optimizer.step()
+
+    loss_epoch.append(loss.item())
+
+  loss_epoch = np.array(loss_epoch).mean()
+  model_ds_factory.append_loss("loss_train", loss_epoch)
+
+  return loss_epoch
+
+
+def valid_ds(model_ds_factory, model_factory, valid_dataset, criterion):
+  valid_dataloader = torch.utils.data.DataLoader(valid_dataset, batch_size = 1, shuffle = False)
+  model_ds_factory.model.eval()
+  model_factory.model.eval()
+  loss_epoch = []
+  for x, target_true in valid_dataloader:
+    features = model_factory.model.features(x)
+    target_pred = model_ds_factory.model(features)
+    loss = criterion(target_pred, target_true)
+    loss_epoch.append(loss.item())
+  loss_epoch = np.array(loss_epoch).mean()
+  
+  model_ds_factory.append_loss("loss_valid", loss_epoch)
+
+  return loss_epoch
+
+
 def train(model_factory, train_dataset, criterion, optimizer):
   train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size = 1, shuffle = True)
   model_factory.model.train()
