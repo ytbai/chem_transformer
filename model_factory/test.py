3d08e2b1fc00924672dfe66386a6990d8904e706
Yuntao Bai <ytbai@princeton.edu>
2020-07-31 12:21:00 -0700

    Test function

---
+++ model_factory/test.py
@@ -0,0 +1,14 @@
+import torch
+import numpy as np
+
+# assume batch size 1
+def test_once(model_factory, test_dataloader, criterion):
+  model_factory.model.eval()
+  loss_epoch = []
+  for x, y_true in test_dataloader:
+    y_pred = model_factory.model(x)
+    loss = criterion(y_pred, y_true)
+    loss_epoch.append(loss.item())
+  loss_epoch = np.array(loss_epoch).mean()
+  
+  return loss_epoch
