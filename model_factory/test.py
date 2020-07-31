6408e794022328fb6bf0f522cc2383c55a56c686
Yuntao Bai <ytbai@princeton.edu>
2020-07-31 15:59:05 -0700

    Incorporate test_delaney() into test()
    Use argument model_factory = "delaney" to return test_delaney()

--- model_factory/test.py
+++ model_factory/test.py
@@ -2,16 +2,22 @@ import torch
 import numpy as np
 
 def test(model_factory, test_dataset, criterion):
-  model_factory.model.eval()
-  test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size = 1, shuffle = False)
-  loss_epoch = []
-  for x, y_true in test_dataloader:
-    y_pred = model_factory.model(x)
-    loss = criterion(y_pred, y_true)
-    loss_epoch.append(loss.item())
-  loss_epoch = np.array(loss_epoch).mean()
-  
-  return loss_epoch
+  if isinstance(model_factory, str) and model_factory == "delaney":
+    return test_delaney(test_dataset, criterion)
+  else:
+    model_factory.model.eval()
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size = 1, shuffle = False)
+    loss_epoch = []
+    for x, y_true in test_dataloader:
+      y_pred = model_factory.model(x)
+      loss = criterion(y_pred, y_true)
+      loss_epoch.append(loss.item())
+    loss_epoch = np.array(loss_epoch).mean()
+
+    return loss_epoch
+
+
+
 
 def test_delaney(test_dataset, criterion):
   test_dataset.set_delaney()
