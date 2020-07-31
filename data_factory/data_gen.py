f10b6bd20ac4a819471cbb2a123de59f4bdb0c90
Yuntao Bai <ytbai@princeton.edu>
2020-07-31 11:02:14 -0700

    Data generator produces csv files with more chemical features based on Duvenaud's data files
    Also produces "delaney_full.csv"

--- data_factory/data_gen.py
+++ data_factory/data_gen.py
@@ -15,18 +15,15 @@ class DelaneyDataset(torch.utils.data.Dataset):
   
   def set_file_address(self):
     data_directory = "data_factory/data"
-    if self.mode == "full":
-      file_name = "delaney.csv"
-    elif self.mode in ["train", "valid", "test"]:
-      file_name = "delaney_"+self.mode+".csv"
+    file_name = "delaney_"+self.mode+".csv"
     self.file_address = os.path.join(data_directory, file_name)
 
   def __getitem__(self, i):
     series              = self.df.iloc[i]
     compound_id         = series[0] 
-    y_true              = torch.tensor(series[1]).type(torch.cuda.FloatTensor)
-    y_esol              = torch.tensor(series[2]).type(torch.cuda.FloatTensor)
-    smiles              = series[3].strip()
+    y_true              = torch.tensor(series[8]).type(torch.cuda.FloatTensor)
+    y_esol              = torch.tensor(series[1]).type(torch.cuda.FloatTensor)
+    smiles              = series[9].strip()
     x                   = self.embed_obj.embed_smiles(smiles).type(torch.cuda.FloatTensor)
     
     return x, y_true, y_esol
@@ -36,17 +33,23 @@ class DelaneyDataset(torch.utils.data.Dataset):
 
 
 def make_split():
-
   data_dir = "data_factory/data"
   file_name = "delaney.csv"
-  file_address = os.path.join(data_dir, file_name)
+  file_name_processed = "delaney-processed.csv"
+  file_name_excluded = "excluded_molecules.csv"
+  file_address_processed = os.path.join(data_dir, file_name_processed)
+  file_address_excluded = os.path.join(data_dir, file_name_excluded)
 
-  df = pd.read_csv(file_address)
+  df_processed = pd.read_csv(file_address_processed)
+  df_excluded = pd.read_csv(file_address_excluded)
+  df = pd.concat([df_processed, df_excluded], ignore_index=True)
+
   df = df.sample(frac=1, random_state=0).reset_index(drop=True)
   df_train = df.iloc[:700]
   df_valid = df.iloc[700:900]
   df_test = df.iloc[900:]
 
+  df.to_csv(os.path.join(data_dir, "delaney_full.csv"), index=False)
   df_train.to_csv(os.path.join(data_dir, "delaney_train.csv"), index=False)
   df_valid.to_csv(os.path.join(data_dir, "delaney_valid.csv"), index=False)
   df_test.to_csv(os.path.join(data_dir, "delaney_test.csv"), index=False)
