bafe530ab20014731ab82ff212f16fe26336235e
Yuntao Bai <ytbai@princeton.edu>
2020-07-31 19:13:46 -0700

    Simplify test_ds() and train_ds() since all DS models are now regressors only

---
+++ model_factory/test_ds.py
@@ -0,0 +1,18 @@
+import torch
+import numpy as np
+
+def test_ds(model_ds, model_factory, test_dataset, metric):
+  test_dataset.set_target(model_ds.target_name)
+  test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size = 1, shuffle = False)
+  model_ds.model.eval()
+  model_factory.model.eval()
+  loss_epoch = []
+  for x, target_true in test_dataloader:
+    features = model_factory.model.features(x).detach()
+    target_pred = model_ds.model(features)
+
+    loss = metric(target_pred, target_true)
+    loss_epoch.append(loss.item())
+  loss_epoch = np.array(loss_epoch).mean()
+
+  return loss_epoch
--- model_factory/train_ds.py
+++ model_factory/train_ds.py
@@ -1,46 +1,42 @@
 import torch
 import numpy as np
 
-def train_ds(ds, model_factory, train_dataset, optimizer):
-  train_dataset.set_target(ds.target_name)
+def train_ds(model_ds, model_factory, train_dataset, criterion, optimizer):
+  train_dataset.set_target(model_ds.target_name)
   train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size = 1, shuffle = True)
-  ds.model.train()
+  model_ds.model.train()
   model_factory.model.eval()
   loss_epoch = []
   for x, target_true in train_dataloader:
     optimizer.zero_grad()
     features = model_factory.model.features(x).detach()
-    target_pred = ds.model(features)
-    loss = ds.criterion(target_pred, target_true)
+    target_pred = model_ds.model(features)
+    loss = criterion(target_pred, target_true)
     loss.backward()
     optimizer.step()
 
     loss_epoch.append(loss.item())
 
   loss_epoch = np.array(loss_epoch).mean()
-  ds.model_factory.append_loss("loss_train", loss_epoch)
+  model_ds.model_factory.append_loss("loss_train", loss_epoch)
 
   return loss_epoch
 
 
-def valid_ds(ds, model_factory, valid_dataset):
-  valid_dataset.set_target(ds.target_name)
+def valid_ds(model_ds, model_factory, valid_dataset, metric):
+  valid_dataset.set_target(model_ds.target_name)
   valid_dataloader = torch.utils.data.DataLoader(valid_dataset, batch_size = 1, shuffle = False)
-  ds.model.eval()
+  model_ds.model.eval()
   model_factory.model.eval()
   loss_epoch = []
   for x, target_true in valid_dataloader:
     features = model_factory.model.features(x).detach()
-    target_pred = ds.model(features)
+    target_pred = model_ds.model(features)
 
-    if isinstance(ds.metric, str) and ds.metric == "acc":
-      loss = int((target_pred == target_true).item())
-      loss_epoch.append(loss)
-    else:
-      loss = ds.metric(target_pred, target_true)
-      loss_epoch.append(loss.item())
+    loss = metric(target_pred, target_true)
+    loss_epoch.append(loss.item())
   loss_epoch = np.array(loss_epoch).mean()
-  
-  ds.model_factory.append_loss("loss_valid", loss_epoch)
+
+  model_ds.model_factory.append_loss("loss_valid", loss_epoch)
 
   return loss_epoch
