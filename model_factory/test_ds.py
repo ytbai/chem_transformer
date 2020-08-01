b386a5bef7a82eb649d746487e6b8184ad79174c
Yuntao Bai <ytbai@princeton.edu>
2020-07-31 21:30:18 -0700

    rename test_ds_mean() to test_ds_const()
    because we also want to test "mode model"

--- model_factory/test_ds.py
+++ model_factory/test_ds.py
@@ -17,7 +17,7 @@ def test_ds(model_ds, model_factory, test_dataset, metric):
 
   return loss_epoch
 
-def test_ds_mean(target_pred, target_name, test_dataset, metric):
+def test_ds_const(target_pred, target_name, test_dataset, metric):
   test_dataset.set_target(target_name)
   test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size = 1, shuffle = False)
 
