fb7d7abb6a288475e6c4a8c17b3b37b0fe14896b
Yuntao Bai <ytbai@princeton.edu>
2020-07-31 20:27:27 -0700

    test_ds_mean() tests mean prediction for DS tasks

--- model_factory/test_ds.py
+++ model_factory/test_ds.py
@@ -16,3 +16,15 @@ def test_ds(model_ds, model_factory, test_dataset, metric):
   loss_epoch = np.array(loss_epoch).mean()
 
   return loss_epoch
+
+def test_ds_mean(target_pred, target_name, test_dataset, metric):
+  test_dataset.set_target(target_name)
+  test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size = 1, shuffle = False)
+
+  loss_epoch = []
+  for x, target_true in test_dataloader:
+    loss = metric(target_pred, target_true)
+    loss_epoch.append(loss.item())
+  loss_epoch = np.array(loss_epoch).mean()
+
+  return loss_epoch
