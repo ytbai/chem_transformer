8bb34ff733b0fd43515955a2f530d5dfed726c47
Yuntao Bai <ytbai@princeton.edu>
2020-07-31 19:05:29 -0700

    Change all DS classifier models to regressors

--- model_factory/models/NumHDonorsRegressor/model.py
+++ model_factory/models/NumHDonorsRegressor/model.py
@@ -3,17 +3,17 @@ import numpy as np
 from torch import nn
 from model_factory.modules import *
 
-class NumRotBondsClassifier(nn.Module):
+class NumHDonorsRegressor(nn.Module):
   def __init__(self):
     super().__init__()
     self.d_model = 64
     self.n_cxt = 97
-    self.num_classes = 24
-    self.head = ClassifierHead(self.d_model, self.n_cxt, self.num_classes)
+    self.num_classes = 12
+    self.head = RegressorHead(self.d_model, self.n_cxt)
 
   def forward(self, x):
     output = self.head(x)
     if self.training:
       return output
     else:
-      return torch.argmax(output, dim = 1, keepdim = False)
+      return torch.round(output)
