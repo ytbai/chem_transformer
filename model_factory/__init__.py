789a1775c0e96f95fe02e02d15b9bddf48346ed5
Yuntao Bai <ytbai@princeton.edu>
2020-07-31 18:27:15 -0700

    Add DS() class to help training and evaluating downstream "DS" models

--- model_factory/__init__.py
+++ model_factory/__init__.py
@@ -3,5 +3,13 @@ from model_factory.model_factory import *
 from model_factory.train import *
 from model_factory.test import *
 
+
 from model_factory.models.ChemTransformer.model import *
+from model_factory.models.MolWeightRegressor.model import *
 from model_factory.models.PolarAreaRegressor.model import *
+from model_factory.models.NumHDonorsClassifier.model import *
+from model_factory.models.NumRingsClassifier.model import *
+from model_factory.models.NumRotBondsClassifier.model import *
+
+from model_factory.model_ds import *
+from model_factory.train_ds import *
--- model_factory/model_ds.py
+++ model_factory/model_ds.py
@@ -1,14 +1,55 @@
 import torch
 import numpy as np
 
-def DSModel(target_name):
-  if target_name == "polar_area":
-    return PolarAreaRegressor()
+from model_factory.model_factory import *
+from model_factory.models.ChemTransformer.model import *
+from model_factory.models.MolWeightRegressor.model import *
+from model_factory.models.PolarAreaRegressor.model import *
+from model_factory.models.NumHDonorsClassifier.model import *
+from model_factory.models.NumRingsClassifier.model import *
+from model_factory.models.NumRotBondsClassifier.model import *
 
-def DSCriterion(target_name):
-  if target_name == "polar_area":
-    return nn.MSELoss()
 
-def DSMetric(target_name):
-  if target_name == "polar_area":
-    return nn.MSELoss()
+class DS():
+  regressors = ["mol_weight", "polar_area"]
+  classifiers = ["num_H_donors", "num_rings", "num_rot_bonds"]
+
+  model_map = {"mol_weight": MolWeightRegressor(),
+               "polar_area": PolarAreaRegressor(),
+               "num_H_donors": NumHDonorsClassifier(),
+               "num_rings": NumRingsClassifier(),
+               "num_rot_bonds": NumRotBondsClassifier(),
+              }
+
+  def __init__(self, target_name):
+    self.target_name = target_name
+    self.set_ds_type()
+    self.set_model()
+    
+    self.set_criterion()
+    self.set_metric()
+
+  def set_ds_type(self):
+    if self.target_name in self.regressors:
+      self.ds_type = "reg"
+    elif self.target_name in self.classifiers:
+      self.ds_type = "class"
+
+
+  def set_model(self):
+    self.model = self.model_map[self.target_name].cuda()
+
+    self.model_factory = ModelFactory(self.model)
+
+  def set_criterion(self):
+    if self.ds_type == "reg":
+      self.criterion = nn.MSELoss()
+    elif self.ds_type == "class":
+      self.criterion = nn.CrossEntropyLoss()
+
+  def set_metric(self):
+    if self.ds_type == "reg":
+      self.metric = nn.MSELoss()
+    elif self.ds_type == "class":
+      self.metric = "acc"
+
