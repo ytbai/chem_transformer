ee9ce44daa2c928387b2992f75378d522f627a69
Yuntao Bai <ytbai@princeton.edu>
2020-07-31 19:14:40 -0700

    Fix typos

--- model_factory/__init__.py
+++ model_factory/__init__.py
@@ -7,9 +7,10 @@ from model_factory.test import *
 from model_factory.models.ChemTransformer.model import *
 from model_factory.models.MolWeightRegressor.model import *
 from model_factory.models.PolarAreaRegressor.model import *
-from model_factory.models.NumHDonorsClassifier.model import *
-from model_factory.models.NumRingsClassifier.model import *
-from model_factory.models.NumRotBondsClassifier.model import *
+from model_factory.models.NumHDonorsRegressor.model import *
+from model_factory.models.NumRingsRegressor.model import *
+from model_factory.models.NumRotBondsRegressor.model import *
 
 from model_factory.model_ds import *
 from model_factory.train_ds import *
+from model_factory.test_ds import *
